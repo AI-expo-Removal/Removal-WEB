b9e38ed3b1ae8ad270bfa7aafb174cab7be4ab93
유지우 <treeyu12345@naver.com>
2024-03-24 23:05:22 +0900

    ⚙️ :: [#3] 기본 세팅

---
+++ lib/core/fonts/removal_text_style.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+
+const String pretendard = 'Pretendard';
+
+class RemovalTextStyle {
+  static TextStyle heading1({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 42,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle heading2({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 32,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle heading3({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 24,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle heading4({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 20,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle subTitle1({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 20,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle subTitle2({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 18,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle subTitle3({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 14,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle body1({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w400,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle body2({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 14,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w400,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle body3({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 10,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle caption1({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w400,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle caption2({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 12,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w400,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle label1({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle label2({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 12,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle button1({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 20,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle button2({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.visible,
+    );
+  }
+
+  static TextStyle button3({
+    required Color color,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 12,
+      fontFamily: pretendard,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.visible,
+    );
+  }
+}
---
+++ lib/core/removal_color.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class RemovalColor {
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+  static Color red = const Color(0xFFDA3B3B);
+
+  static Color main10 = const Color(0xFFD2FFF0);
+  static Color main20 = const Color(0xFFB4FFF5);
+  static Color main30 = const Color(0xFF98FFFF);
+  static Color main40 = const Color(0xFF7DEDFF);
+  static Color main50 = const Color(0xFF62C9FF);
+  static Color main60 = const Color(0xFF499CFF);
+  static Color main70 = const Color(0xFF3067FF);
+
+  static Color gray10 = const Color(0xFFFAFAFA);
+  static Color gray20 = const Color(0xFFE0E0E0);
+  static Color gray30 = const Color(0xFFC7C7C7);
+  static Color gray40 = const Color(0xFFADADAD);
+  static Color gray50 = const Color(0xFF949494);
+  static Color gray60 = const Color(0xFF7A7A7A);
+  static Color gray70 = const Color(0xFF616161);
+  static Color gray80 = const Color(0xFF474747);
+  static Color gray90 = const Color(0xFF2E2E2E);
+  static Color gray100 = const Color(0xFF141414);
+  static Color gray300 = const Color(0xFFADADAD);
+
+  static Gradient main = LinearGradient(
+    colors: [
+      main70,
+      main50,
+    ],
+  );
+}
